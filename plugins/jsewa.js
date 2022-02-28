64a56ba3cc46dad68287c4bd63aba12cf0763f62
Putbotz <putbot604@gmail.com>
2022-02-28 18:37:35 +0800

    put

---
+++ plugins/jsewa.js
@@ -0,0 +1,23 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `
+joke doang banh gw ga open jasa sewa 🥸
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*✧──────────···──────────✧*\n                   Putbotz Ganteng`, 'status@broadcast')
+}
+handler.help = ['payment']
+handler.tags = ['info']
+handler.command = /^(payment)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
